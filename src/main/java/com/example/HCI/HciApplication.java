089ab2bed4fa2aaf5c370b26fd4b9cb36e468f70
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-11 23:21:15 +0600

    Initial commit

---
+++ src/main/java/com/example/HCI/HciApplication.java
@@ -0,0 +1,12 @@
+package com.example.HCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HciApplication.class, args);
+	}
+}
