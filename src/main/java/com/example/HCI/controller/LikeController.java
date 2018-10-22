046e0adc11e0711a19a3735a976ca45764ef4b47
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-22 23:57:34 +0600

    ajax for likes

--- src/main/java/com/example/HCI/controller/LikeController.java
+++ src/main/java/com/example/HCI/controller/LikeController.java
@@ -4,11 +4,16 @@ import com.example.HCI.model.Likes;
 import com.example.HCI.service.LikeService;
 import com.example.HCI.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import java.util.Calendar;
 
 @Transactional
 @Controller
@@ -20,7 +25,7 @@ public class LikeController {
     @Autowired
     PlaceService placeService;
 
-    @GetMapping(value = "/addLike")
+    @RequestMapping(value = "/addLike", method = RequestMethod.GET, produces = "application/json")
     public String getLike(@RequestParam("id") long id, @RequestParam("username") String username) {
         likeService.save(new Likes(username, id));
         placeService.updateLikes(id,1);
@@ -31,6 +36,16 @@ public class LikeController {
         likeService.removeByUsernameAndAppId(username,id);
         placeService.updateLikes(id,-1);
         return "redirect:/placeInfo?id="+id+"#likes";
+    }
+
+    @RequestMapping(value = "/getFeedBack", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getAction(@RequestParam String text, @RequestParam String feedback, @RequestParam String id, HttpServletRequest request) {
+        AjaxResponseBody result = new AjaxResponseBody();
+        String ip=historyDAO.getIP(request);
+        History history=new History(ip, Calendar.getInstance().getTime(),text,feedback,"feedback",id);
+        historyRepository.save(history);
+        result.setMsg("feedbackThanks");
+        return ResponseEntity.ok(result);
     }
 
 }
