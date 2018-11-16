047e697767b6cf524fa2aecf9813f899482ab004
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-16 23:45:39 +0600

    Profile edited

--- src/main/java/com/example/HCI/controller/MainController.java
+++ src/main/java/com/example/HCI/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.example.HCI.controller;
 
+import com.example.HCI.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 
 @Controller
 @Transactional
 public class MainController {
 
+    @Autowired
+    PlaceService placeService;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        ArrayList list= (ArrayList)placeService.getTop3PlaceByOrderByView();
+        model.addAttribute("places",list);
         return "index";
     }
 }
