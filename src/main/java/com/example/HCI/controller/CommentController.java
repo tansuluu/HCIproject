a1c009ad7b01203ed4eb4f1ffbfaf644d2140431
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-25 18:14:01 +0600

    when г put button it updates automaticaly

--- src/main/java/com/example/HCI/controller/CommentController.java
+++ src/main/java/com/example/HCI/controller/CommentController.java
@@ -31,28 +31,8 @@ public class CommentController {
     @Autowired
     PlaceService placeService;
 
-    @RequestMapping(value = "/newComment", method = RequestMethod.POST)
-    public String saveComment(@ModelAttribute("comment") @Valid Comment comment, BindingResult result, Principal principal, @RequestParam("appId") long appId) {
-        if (result.hasErrors()) {
-            return "redirect:/placeInfo?id=" + appId + "#comment";
-        }
-        comment.setUsername(principal.getName());
-        comment.setIdPlace(appId);
-        comment.setImage(userService.findUserByEmail(principal.getName()).getImage());
-        commentService.save(comment);
-        placeService.updateCommentNum(comment.getIdPlace(), 1);
-        return "redirect:/placeInfo?id=" + appId + "#comment";
-    }
-
-    @RequestMapping("/deleteComment")
-    public String deleteComment(@RequestParam("id") long id, @RequestParam("apId") long appId) {
-        placeService.updateCommentNum(appId, -1);
-        commentService.deleteById(id);
-        return "redirect:/appInfo?id=" + appId + "#comment";
-    }
-
     @RequestMapping(value = "/newComment", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<?> getLike(@RequestParam("comment") String comment,@RequestParam("appId") long appId, Principal principal) {
+    public ResponseEntity<?> addComment(@RequestParam("comment") String comment,@RequestParam("appId") long appId, Principal principal) {
         System.out.print("hererererrerrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
         return ResponseEntity.ok(7);
     }
