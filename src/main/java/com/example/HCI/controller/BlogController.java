44dcc705f08e0243adf18a7dc6c48822e7b18de1
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2018-12-04 17:28:41 +0600

    Blog Repository

---
+++ src/main/java/com/example/HCI/controller/BlogController.java
@@ -0,0 +1,10 @@
+package com.example.HCI.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+@Controller
+@Transactional
+public class BlogController {
+}
---
+++ src/main/java/com/example/HCI/repository/BlogRepository.java
@@ -0,0 +1,16 @@
+package com.example.HCI.repository;
+
+import com.example.HCI.model.Blog;
+import com.example.HCI.model.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog,Long> {
+    Blog findById(long id);
+    List<Blog> getAllByOrderByView();
+    List<Blog> findBlogByUsername(String username);
+
+}
