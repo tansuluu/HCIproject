1b9310a9e190ad79d0e231ba4574ca4249df2b04
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2018-12-08 23:11:03 +0600

    Index page, blogs added

--- src/main/java/com/example/HCI/repository/BlogRepository.java
+++ src/main/java/com/example/HCI/repository/BlogRepository.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface BlogRepository extends JpaRepository<Blog,Long> {
     Blog findById(long id);
     List<Blog> getAllByOrderByView();
-    List<Blog> getAllOrOrderByDate();
     List<Blog> findBlogByUsername(String username);
 
 }
